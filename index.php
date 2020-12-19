710c9bfd6dae572ade220c66044670b7a73f1fea
gertjanvdt <64344880+gertjanvdt@users.noreply.github.com>
2020-12-19 13:39:13 +0100

    updates

--- index.php
+++ index.php
@@ -7,38 +7,50 @@ $movies = (object) array(
         "description" => "It’s more than a little far-fetched to think that any parents would completely forget about their 8-year-old, and even less likely that said 8-year-old could actually take on a pair of burglars, but once you move past the premise, this movie is classic Christmas entertainment.",
         "image" => "images/home_alone.jpeg",
         "rating" => 4,
+        "discount" => false,
+        "price" => 5.99
     ),
     "movie_2" => (object) array(
         "title" => "Love Actually",
         "actors" => "Hugh Grant, Martine McCutcheon, Liam Neeson",
         "description" => "Take an all-star cast, eight rom-com storylines, and Christmas, and what do you get? An incredible ensemble movie—that Hollywood has since tried (unsuccessfully) to replicate with the truly awful Valentine’s Day and what looks like an even worse New Year’s Eve.",
         "image" => "images/love_actually.jpg",
-        "rating"=> 2
+        "rating"=> 2,
+        "discount" => true,
+        "price" => 9.99
     ),
     "movie_3" => (object) array(
         "title" => "National Lampoon’s Christmas Vacation",
         "actors" => "Chevy Chase, Beverly D'Angelo, Juliette Lewis ",
         "description" => "Chevy Chase was at his peak as the head of the Griswold household, determined to give his family an old-fashioned Christmas. But despite his best efforts, things start to go awry as soon as his extended family arrives. ",
         "image" => "images/christmas_vacation.jpg",
-        "rating"=> 3
+        "rating"=> 3,
+        "discount" => true,
+        "price" => 7.50
     ),
     "movie_4" => (object) array(
         "title" => "Elf",
         "actors" => "Will Ferrell, James Caan, Bob Newhart",
         "description" => "If you haven’t seen this yet, don’t let the old-school animation at the beginning of the film put you off—the movie aims to make use of older animated Christmas classics. Whether you watch with your nieces and nephews or your parents, Will Ferrell is endearingly earnest as Buddy, even in the face of the cynical reality of New York City. ",
         "image" => "images/elf.jpg",
-        "rating"=> 5
+        "rating"=> 5,
+        "discount" => false,
+        "price" => 11.50
     ),
     "movie_5" => (object) array(
         "title" => "A Christmas Story",
         "actors" => "Peter Billingsley, Melinda Dillon, Darren McGavin",
         "description" => "This staple is ranked consistently as one of the best holiday films of all time, and for good reason. The sheer number of memorable scenes and lines—Flick getting his tongue stuck to the flagpole, the boys being triple-dog-dared to do something (and the aghast look on Ralphie’s face as the dares escalate), Ralphie’s cringe-inducing pink bunny pajamas, eating a Christmas dinner of Peking duck (or what Ralphie calls a Chinese turkey), and of course the iconic line, “You’ll shoot your eye out!” ",
         "image" => "images/christmas_story.jpg",
-        "rating"=> 2
-    ),
-
-    // add method for discount
+        "rating"=> 2,
+        "discount" => true,
+        "price" => 12.99
+    )
 );
+
+function calculateDiscount($price) {
+    return $price * 0.9;
+};
 
 $key = 0; 
 
@@ -95,8 +107,23 @@ $key = 0;
                             };
                             ?>
                         </p>
-                        <button type="button">More info</button>
-                        <button type="button">Add to cart</button>
+                        <div>
+                            <!-- Buttons -->
+                            <button type="button">More info</button>
+                            <button type="button" id="add_to_cart">Add to cart</button>
+                        </div>
+                        <div>
+                            <!-- price info -->
+                            <?php
+                                if ($movie->discount == true) {
+                                    $newPrice = calculateDiscount($movie->price);             
+                            ?>      
+                                <p>From: $<?php echo $movie->price ?></p>
+                                <p>For: $<?php echo $newPrice?></p>
+                            <?php }; ?>
+
+                        </div>
+
                     </div>
                     <div class="movie_image">
                         <img src="<?php echo $movie->image?>" alt="movie cover image" id="movie_image">
